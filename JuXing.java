92c28ca1a3c42bfe3ab150f202a9f62974da623b
hooware <1411450483@qq.com>
2018-10-20 15:45:23 +0800

    ShellSort Arithmetic

---
+++ JuXing.java
@@ -0,0 +1,67 @@
+package lianxi.day04;
+
+public class JuXing {
+    public static void main(String[] args) {
+        kk3();
+    }
+
+    public static void kk() {
+        for (int i = 1; i <= 5; i++) {
+            for (int k = 1; k <= 8; k++) {
+                if (i > 1 && i < 5 && k > 1 && k < 8) {
+                    System.out.print(" ");
+                    continue;
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ff() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 8; j++) {
+                if (i == 2 && j > 1) {
+                    System.out.print(" ");
+                    continue;
+                } else if (i == 4 && j < 8) {
+                    System.out.print(" ");
+                    continue;
+                } else
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void kk3() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 32; j++) {   //定义个5行20列的循环
+                if (i == 1 && j > 8 && j < 13 || j > 20 && j < 24) { //定义第一行输出条件
+                    System.out.print(" ");  //满足条件的输出空格
+                    continue;
+                } else if (i == 2 && j > 1 && j < 20 || i == 2 && j > 20 && j < 24 || i == 2 && j > 24 && j < 32) { //定义第二行
+                    System.out.print(" "); //输出空格
+                    continue;
+                } else if (i == 3 && j > 8 && j < 13 || i == 3 && j > 20 && j < 24 || i == 3 && j > 24 && j < 32) { //定义第三行
+                    System.out.print(" ");
+                    continue;
+                } else if (i == 4 && j < 8 || i == 4 && j > 8 & j < 13 || i == 4 & j > 13 & j < 24 || i == 4 && j > 24 & j < 32) {
+                    System.out.print(" ");
+                    continue;
+                } else if (i == 5 && j > 8 && j < 13|| j > 20 && j < 24) {
+                    System.out.print(" ");
+                    continue;
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
+
+
+
+
--- ShellSort.java
+++ ShellSort.java
@@ -1,11 +1,27 @@
 // 插入希尔算法测试
 public class ShellSort {
     public static void main(String[] args) {
-        
+        // 静态初始化一个乱序数组arr
+        int[] arr = new int[] { 12, 4, 67, 2, 6, 2, 9, 5, 3, 23, 54 };
+        shell(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
     }
 
     public static void shell(int[] arr) {
         int increment = arr.length;
-        
+        do {
+            increment = increment / 3 + 1;
+            for (int i = increment; i < arr.length; i++) { // 从间隔increment之后开始往后依次与前increment间隔的数据比较，小的则交换到前面
+                for (int j = i; j >= increment; j -= increment) { // 将每个以increment间隔分割的小数组有序化
+                    if (arr[j] < arr[j - increment]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j - increment];
+                        arr[j - increment] = temp;
+                    }
+                }
+            }
+        } while (increment > 1);
     }
 }
